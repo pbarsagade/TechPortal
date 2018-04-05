726fdbb153c4e4c467317aee64a70ca9aa96e620
pbarsagade <prakash.barsagade@outlook.com>
2018-04-05 09:44:38 +0530

    folder structure setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,16 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { QuestionListComponent } from './questions/question-list/question-list.component';
-import { QuestionDetailComponent } from './questions/question-detail/question-detail.component';
-import { PostQuestionComponent } from './questions/post-question/post-question.component';
-import { PostAnswerComponent } from './questions/post-answer/post-answer.component';
+import { QuestionListComponent } from './home/questions/question-list/question-list.component';
+import { QuestionDetailComponent } from './home/questions/question-detail/question-detail.component';
+import { PostQuestionComponent } from './home/questions/post-question/post-question.component';
+import { PostAnswerComponent } from './home/questions/post-answer/post-answer.component';
 import { ProfileComponent } from './user/profile/profile.component';
 import { MyQuestionsComponent } from './user/my-questions/my-questions.component';
 import { MyAnswersComponent } from './user/my-answers/my-answers.component';
 import { MyTodoComponent } from './user/my-todo/my-todo.component';
 import { ProfileDetailComponent } from './user/profile-detail/profile-detail.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { ProfileDetailComponent } from './user/profile-detail/profile-detail.com
     MyQuestionsComponent,
     MyAnswersComponent,
     MyTodoComponent,
-    ProfileDetailComponent
+    ProfileDetailComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
