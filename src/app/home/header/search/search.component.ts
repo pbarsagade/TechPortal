fe79fb9fdb266db554b228f33ff176c7fd569508
pbarsagade <prakash.barsagade@outlook.com>
2018-05-13 03:13:40 +0530

    Search redirect to home page

--- src/app/home/header/search/search.component.ts
+++ src/app/home/header/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { QuestionService } from '../../questions/services/question.service';
 import { SearchFilterService } from '../../search-filter.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -8,11 +9,14 @@ import { SearchFilterService } from '../../search-filter.service';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  constructor(private searchSvc: SearchFilterService) { }
+  constructor(private searchSvc: SearchFilterService, private router: Router) { }
   searchText: string;
   ngOnInit() {
   }
   onSearch(): void {
     this.searchSvc.filter.next(this.searchText);
+    if (this.router.url.indexOf('/') >= 0) {
+      this.router.navigate(['']);
+    }
   }
 }
